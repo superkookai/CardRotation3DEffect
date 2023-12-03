8d8c38884b33a818d1f969a7320764bc1ed383ef
Weerawut Chaiyasomboon <weerawut@live.com>
2023-12-03 13:35:35 +0700

    Initial Commit

---
+++ CardRotation3DEffect/CardRotation3DEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardRotation3DEffectApp.swift
+//  CardRotation3DEffect
+//
+//  Created by Weerawut Chaiyasomboon on 3/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CardRotation3DEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardRotation3DEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardRotation3DEffect
+//
+//  Created by Weerawut Chaiyasomboon on 3/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
